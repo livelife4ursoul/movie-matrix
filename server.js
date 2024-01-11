4bea58da903931bdd33856c0f83d6d0749bf009a
livelife4ursoul <livelife4ursoul@gmail.com>
2024-01-10 17:26:52 -0800

    Updated documents
    fixed the filePath code for server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require('http'),
 http.createServer((request, response) => {
     let addr = request.url,
     q = new URL(addr, 'http://' + request.headers.host),
-    filePath = ' ';
+    filePath = '';
 
     fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
         if (err) {
@@ -18,18 +18,55 @@ http.createServer((request, response) => {
     if (q.pathname.includes('documentation')) {
         filePath = (__dirname + '/documentation.html');
     } else {
-        filePath = 'index.html';
+        filePath = (__dirname + '/index.html');
     }
 
     fs.readFile(filePath, (err, data) => {
         if (err) {
             throw err;
         }
-    
+
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
         response.end();
+
     });
 
-}).listen(8080);
-console.log('My first Node test server is running on Port 8080');
+   }).listen(8080);
+   console.log('My first Node test server is running on Port 8080');
+
+// const http = require('http'),
+//     fs = require('fs'),
+//     url = require('url');
+
+// http.createServer((request, response) => {
+//     let addr = request.url,
+//     q = new URL(addr, 'http://localhost:8080' + request.headers.host),
+//     filePath = '';
+
+//     fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log('Added to log.');
+//         }
+//     });
+
+//     if (q.pathname.includes('documentation')) {
+//         filePath = (__dirname + '/documentation.html');
+//     } else {
+//         filePath = 'index.html';
+//     }
+
+//     fs.readFile(filePath, (err, data) => {
+//         if (err) {
+//             throw err;
+//         }
+    
+//         response.writeHead(200, {'Content-Type': 'text/html'});
+//         response.write(data);
+//         response.end();
+//     });
+
+// }).listen(8080);
+// console.log('My first Node test server is running on Port 8080');
